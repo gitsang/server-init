4b96bf7c2e23a5b61e2cab4e234b40c5c8a645ba
gitsang <sang.chen@outlook.com>
2022-10-08 16:18:10 +0800

    update

--- init.sh
+++ init.sh
@@ -15,21 +15,39 @@ install_node() {
 }
 
 install_git() {
+    CONFIG_PATH=./git
+
+    # install
     apt install -y git
     apt autoremove -y
-    cp .gitconfig ~
+
+    # configure
+    cp ${CONFIG_PATH}/.gitconfig ~
     npm install -g git-split-diffs
 }
 
-install_vim() {
-    apt install -y vim neovim python3-neovim
-    apt autoremove -y
+install_nvim() {
+    VERSION=v0.8.0
+    CONFIG_PATH=./nvim
+
+    # download and install
+    wget -c https://github.com/neovim/neovim/releases/download/${VERSION}/nvim.appimage
+    chmod u+x nvim.appimage
+    mv nvim.appimage /usr/local/bin/nvim
+
+    # confignure
     mkdir -p ~/.config/nvim/
-    cp init.vim ~/.config/nvim/
-    cp coc-settings.json ~/.config/nvim/
+    cp ${CONFIG_PATH}/init.vim ~/.config/nvim/
+    cp ${CONFIG_PATH}/coc-settings.json ~/.config/nvim/
+
+    # start
+    /usr/local/bin/nvim
 }
 
 install_zsh() {
+    CONFIG_PATH=./shell
+
+    # install
     apt install -y zsh
     apt autoremove -y
     if [ ! -d "~/.zsh/plugins/zsh-autosuggestions" ]; then
@@ -38,10 +56,12 @@ install_zsh() {
     if [ ! -d "~/.zsh/plugins/zsh-syntax-highlighting" ]; then
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/plugins/zsh-syntax-highlighting
     fi
-    cp .shrc ~
-    cp .zshrc ~
-    cp .bashrc ~
-    cp .bash_aliases ~
+
+    # configure
+    cp ${CONFIG_PATH}/.shrc ~
+    cp ${CONFIG_PATH}/.zshrc ~
+    cp ${CONFIG_PATH}/.bashrc ~
+    cp ${CONFIG_PATH}/.bash_aliases ~
     chsh -s $(which zsh)
 }
 
@@ -51,19 +71,59 @@ install_fzf() {
 }
 
 install_docker() {
+    # install
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
+
+    # configure
     mkdir -p ~/.zsh/completion
     curl \
         -L https://raw.githubusercontent.com/docker/compose/v2.5.0/contrib/completion/zsh/_docker-compose \
         -o ~/.zsh/completion/_docker-compose
-    apt autoremove -y
 }
 
-install_go() {
-    GO_VERSION=1.18.2
-    TGZ_FILE=go${GO_VERSION}.linux-amd64.tar.gz
-    wget https://go.dev/dl/${TGZ_FILE}
-    tar -C /usr/local -xzvf ${TGZ_FILE}
-    rm -fr ${TGZ_FILE}
+show_help() {
+    # usage
+    echo "Usage: ${0} Option"
+
+    # options
+    echo ""
+    echo "Option:"
+    grep -E '\s+[a-zA-Z_-]+\).*##' $0 | \
+        sed -r 's/\s+([a-zA-Z_-]+)\).*## (.*)/\1|\2/g' | \
+        awk -F '|' '{printf "\t\033[36m%-20s\033[0m %s\n", $1, $2}'
+
+    # config
+    echo ""
+    echo "Config:"
+    grep -E '^([0-9a-zA-Z_-]+)=\$\{\1:-.*\}' $0 | \
+        sed -r 's/^([0-9a-zA-Z_-]+)=\$\{\1:-\"?([^\"]*)\"?\}/\1|\2/g' | \
+        awk -F '|' '{printf "\t\033[33m%-16s\033[0m(default: %s)\n", $1, $2}'
 }
+
+case $1 in
+    basic) ## install basic
+        basic
+        ;;
+    node) ## install node
+        install_node
+        ;;
+    git) ## install git
+        install_git
+        ;;
+    nvim) ## install nvim using appimage
+        install_nvim
+        ;;
+    zsh) ## install zsh
+        install_zsh
+        ;;
+    fzf) ## install fzf
+        install_fzf
+        ;;
+    docker) ## install docker
+        install_docker
+        ;;
+    *)
+        show_help
+        ;;
+esac
