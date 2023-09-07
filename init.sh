49a01009807f8428e4cfb94db21cd2ece1e36161
sang <sang.chen@outlook.com>
2023-09-07 02:46:39 +0000

    fix: fix fzf

--- init.sh
+++ init.sh
@@ -186,7 +186,9 @@ install_zsh() {
 # ========================= fzf ========================= #
 
 install_fzf_from_source() {
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    if [ ! -d ~/.fzf ]; then
+        git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    fi
     ~/.fzf/install
 }
 
