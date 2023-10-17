ad744001ec2990e5f576b094c836d69f82c3b0a9
sang <sang.chen@outlook.com>
2023-10-17 09:55:54 +0000

    update init

--- init.sh
+++ init.sh
@@ -54,6 +54,7 @@ install_prerequisite() {
         sudo apt install -y zlib1g-dev
         sudo apt install -y asciidoc
         sudo apt install -y xmlto
+        sudo apt install -y libcurl4-openssl-dev
 
         sudo apt autoremove -y
     fi
